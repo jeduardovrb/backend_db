b3c3116d5b4e16c92418ef4f47dc14397cfb2ab9
unknown <jorgeeduardovrb@gmail.com>
2021-03-29 20:35:14 -0300

    adicionando novos arquivos

---
+++ models/pessoa.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PessoaSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    senha: {
+        type: String,
+        required: true 
+    },
+    usename: {
+        type: String
+    },
+    date: {
+        type: String,
+        default: Date.now()
+    },
+});
+
+module.exports = Pessoa = mongoose.model("pessoa", PessoaSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+// tratar aitenticação
+
+const Pessoa = require("../models/pessoa");
+
+router.post("/signup", (req, res) => {
+    Pessoa.findOne({email: req.body.email})
+    .then(doc_pessoa => {
+        if (doc_pessoa) {  
+            return res.status(400).json({emailerro: "Email já registrado no sistema"});
+        } else {
+            const novo_reg_pessoa = Pessoa({
+                name: req.body.name,
+                email: req.body.email,
+                senha: req.body.senha,
+                usename: req.body.usename,
+            });
+            // salvar no bd
+            novo_reg_pessoa
+            .save()
+            .then(p => res.json(p))
+            .catch(err => console.log(err));
+        }
+    })
+    .catch(err => console.log(err));
+});
+
 router.get("/", (req, res) => res.json({status: "Acessp permitido"}));
 
 module.exports = router;
